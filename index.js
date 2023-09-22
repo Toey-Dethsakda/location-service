12737069f30fbe687e09cc3b6353b8089fb070cd
Toey-Dethsakda <arigato.toey@gmail.com>
2023-09-22 19:30:51 +0700

    allow core

--- index.js
+++ index.js
@@ -12,8 +12,16 @@ connectDB()
 
 const PORT = process.env.PORT
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your actual frontend origin
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.get('/api/locations', (req, res, next) => {
@@ -47,5 +55,5 @@ app.post('/api/locations', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`)
+    console.log(`Example app listening on port ${PORT}`)
 })
