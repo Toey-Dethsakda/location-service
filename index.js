7e851eeb55b40a06c5ff2fdd4a4ac37268e9f5ec
Toey-Dethsakda <arigato.toey@gmail.com>
2023-09-22 21:24:58 +0700

    allow more

--- index.js
+++ index.js
@@ -8,24 +8,16 @@ const Location = require('./models/location')
 app.use(express.json())
 
 app.use(cors({
-    origin: 'https://locat-storie.vercel.app/', // Allow all origins (you can restrict it to specific domains if needed)
-    methods: 'GET, POST, PUT, DELETE, OPTIONS', // Allow the specified HTTP methods
-    allowedHeaders: 'Content-Type', // Allow the specified headers
-  }));
+    origin: ['https://locat-storie.vercel.app', 'https://another-allowed-origin.com', 'https://yet-another-allowed-origin.com'],
+    methods: 'GET, POST, PUT, DELETE, OPTIONS',
+    allowedHeaders: 'Content-Type',
+}));
 
 const connectDB = require('./connectMongo')
 
 connectDB()
 
 const PORT = process.env.PORT
-
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '(*)'); // Replace with your actual frontend origin
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
-
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
